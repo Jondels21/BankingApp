f40e77a5cb5f430500fccb8c92e07846b2ccf079
Janne Salovaara <jannesalovaara@gmail.com>
2026-06-06 01:38:26 +0300

    Classes for Accounts and Users

---
+++ Models/Account.cs
@@ -0,0 +1,9 @@
+namespace BankingApp;
+
+public class Account
+{
+    public int Id { get; set; }
+    public int UserId {get; set; }
+    public string Name { get; set; } = string.Empty;
+    public decimal Balance { get; set; }
+}
