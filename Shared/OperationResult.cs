edff4e6d4c374f3e11df54de0af0f6ccb87c1b2d
Janne Salovaara <jannesalovaara@gmail.com>
2026-06-27 16:59:19 +0300

    Added result checking for operations with data

--- Shared/OperationResult.cs
+++ Shared/OperationResult.cs
@@ -13,3 +13,18 @@ public class OperationResult
         => new() {Success = false, Message = message };
 
 }
+
+public class OperationResult<T>
+{
+    public bool Success {get; init; }
+    public string Message {get; init; } = string.Empty;
+    public T? Data { get; init; }
+
+
+    public static OperationResult<T> Ok(T data, string message = "")
+        => new() {Success = true, Message = message, Data = data};
+
+    public static OperationResult<T> Fail(string message)
+        => new() {Success = false, Message = message, Data = default};
+        
+}
